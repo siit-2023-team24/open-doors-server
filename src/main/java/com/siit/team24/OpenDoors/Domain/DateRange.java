67b2a1a86a55ac635c760ea62810f8147210690d
Sonja-Parabucki <105170152+Sonja-Parabucki@users.noreply.github.com>
2023-11-20 20:59:59 +0100

    added reservation request model

---
+++ src/main/java/com/siit/team24/OpenDoors/Domain/DateRange.java
@@ -0,0 +1,7 @@
+package com.siit.team24.OpenDoors.Domain;
+
+import jakarta.persistence.Embeddable;
+
+@Embeddable
+public class DateRange {
+}
---
+++ src/main/java/com/siit/team24/OpenDoors/Domain/Enums/ReservationRequestStatus.java
@@ -0,0 +1,8 @@
+package com.siit.team24.OpenDoors.Domain.Enums;
+
+public enum ReservationRequestStatus {
+    PENDING,
+    CONFIRMED,
+    DENIED,
+    CANCELED
+}
