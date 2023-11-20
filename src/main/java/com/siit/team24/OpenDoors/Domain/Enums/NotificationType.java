c6fda1e911efa497136480477b11b32e000c8e5c
Sonja-Parabucki <105170152+Sonja-Parabucki@users.noreply.github.com>
2023-11-20 21:30:18 +0100

    improved enums

--- src/main/java/com/siit/team24/OpenDoors/Domain/Enums/NotificationType.java
+++ src/main/java/com/siit/team24/OpenDoors/Domain/Enums/NotificationType.java
@@ -1,7 +1,18 @@
 package com.siit.team24.OpenDoors.Domain.Enums;
 
 public enum NotificationType {
-    RESERVATION_REQUEST,
-    HOST_RATING,
-    ACCOMMODATION_RATING
+    NEW_RESERVATION_REQUEST("New reservation request!"),
+    RESERVATION_REQUEST("Reservation request status changed."),
+    HOST_REVIEW("You have a new review."),
+    ACCOMMODATION_REVIEW("Your accommodation was reviewed.");
+
+    private final String typeMessage;
+
+    NotificationType(String typeMessage) {
+        this.typeMessage = typeMessage;
+    }
+
+    public String getTypeMessage() {
+        return typeMessage;
+    }
 }
--- src/main/java/com/siit/team24/OpenDoors/Domain/Enums/ReservationRequestStatus.java
+++ src/main/java/com/siit/team24/OpenDoors/Domain/Enums/ReservationRequestStatus.java
@@ -1,8 +1,18 @@
 package com.siit.team24.OpenDoors.Domain.Enums;
 
 public enum ReservationRequestStatus {
-    PENDING,
-    CONFIRMED,
-    DENIED,
-    CANCELED
+    PENDING("pending"),
+    CONFIRMED("confirmed"),
+    DENIED("denied"),
+    CANCELED("canceled");
+
+    private final String status;
+
+    ReservationRequestStatus(String status) {
+        this.status = status;
+    }
+
+    public String getStatus() {
+        return status;
+    }
 }
